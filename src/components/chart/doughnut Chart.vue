b9b588dd97f455562f08171e19403556f751aca8
Alasher2023 <ly.development2023@gmail.com>
2023-11-11 20:25:01 -0800

    add chart

---
+++ src/components/chart/doughnut Chart.vue
@@ -0,0 +1,50 @@
+
+<template>
+    <div class="card flex justify-content-center">
+        <Chart type="doughnut" :data="chartData" :options="chartOptions" style="width: 50%;" />
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import Chart from 'primevue/chart';
+
+onMounted(() => {
+    chartData.value = setChartData();
+    chartOptions.value = setChartOptions();
+});
+
+const chartData = ref();
+const chartOptions = ref(null);
+
+const setChartData = () => {
+    const documentStyle = getComputedStyle(document.body);
+
+    return {
+        labels: ['A', 'B', 'C'],
+        datasets: [
+            {
+                data: [540, 325, 702],
+                backgroundColor: [documentStyle.getPropertyValue('--blue-500'), documentStyle.getPropertyValue('--yellow-500'), documentStyle.getPropertyValue('--green-500')],
+                hoverBackgroundColor: [documentStyle.getPropertyValue('--blue-400'), documentStyle.getPropertyValue('--yellow-400'), documentStyle.getPropertyValue('--green-400')]
+            }
+        ]
+    };
+};
+
+const setChartOptions = () => {
+    const documentStyle = getComputedStyle(document.documentElement);
+    const textColor = documentStyle.getPropertyValue('--text-color');
+
+    return {
+        plugins: {
+            legend: {
+                labels: {
+                    cutout: '60%',
+                    color: textColor
+                }
+            }
+        }
+    };
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
 import homeView from '@/components/layout/HomeLayout/homeLayout_index.vue'
+import Card from 'primevue/card';
+import Divider from 'primevue/divider';
+import combo_chart from '@/components/chart/comboChart.vue'
+import horizon_chart from '@/components/chart/HorizontalBarChart.vue'
+import doughnut_chart from '@/components/chart/doughnut Chart.vue'
 </script>
 
 <template>
@@ -24,32 +29,60 @@ import homeView from '@/components/layout/HomeLayout/homeLayout_index.vue'
           <div class="col-12">
             <div class="grid">
               <div class="col-12 md:col-6">
-                <div class="h-20rem md:h-full p-3 text-center border-round-sm bg-primary font-bold">
-                  SelesInfo
-                </div>
+                <Card>
+                  <template #header>
+                    <p class="ml-auto bg-blue-100">sales</p>
+                  </template>
+                  <template #title>
+                    <p class="mr-auto bg-yellow-100">sales</p>
+                  </template>
+                  <template #subtitle>
+                    <p class="mr-auto bg-green-100">sales</p>
+                    <Divider></Divider>
+                    <p class="mr-auto bg-green-100">sales</p>
+                  </template>
+                  <template #content>
+                      <p class="m-0 bg-bluegray-100">
+                          sales
+                      </p>
+                  </template>
+              </Card>
               </div>
               <div class="col-12 md:col-6">
                 <div class="grid">
                   <div class="col-12">
-                    <div
-                      class="h-20rem md:h-10rem p-3 text-center border-round-sm bg-primary font-bold"
-                    >
-                      Chart1
-                    </div>
+                    <Card>
+                  <template #title>
+                    <p class="mr-auto bg-yellow-100">chart1</p>
+                  </template>
+                  <template #content>
+                      <doughnut_chart/>
+                  </template>
+              </Card>
                   </div>
                   <div class="col-12">
-                    <div
-                      class="h-20rem md:h-10rem p-3 text-center border-round-sm bg-primary font-bold"
-                    >
-                      Chart2
-                    </div>
+                    <Card>
+                  <template #title>
+                    <p class="mr-auto bg-yellow-100">chart2</p>
+                  </template>
+                  <template #content>
+                    <horizon_chart/>
+                  </template>
+              </Card>
                   </div>
                 </div>
               </div>
             </div>
           </div>
           <div class="col-12">
-            <div class="h-20rem text-center p-3 border-round-sm bg-primary font-bold">Chart3</div>
+            <Card>
+                  <!-- <template #title>
+                    <p class="mr-auto bg-yellow-100">chart1</p>
+                  </template> -->
+                  <template #content>
+                      <combo_chart/>
+                  </template>
+              </Card>
           </div>
         </div>
       </main>
