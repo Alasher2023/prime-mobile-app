bde263aeef872c27a48d73ef3760449271dccb5b
Yang Li <143738908+Alasher2023@users.noreply.github.com>
2023-11-13 09:08:44 +0000

    ios禁止双击放大

--- src/App.vue
+++ src/App.vue
@@ -2,36 +2,37 @@
 import { RouterView } from 'vue-router'
 
 // 禁止IOS放大 Start
-document.documentElement.addEventListener(
-  'touchstart',
-  function (event) {
+window.onload = function () {
+  document.addEventListener('touchstart', function (event) {
     if (event.touches.length > 1) {
       event.preventDefault()
     }
-  },
-  false
-)
-var lastTouchEnd = 0
-document.documentElement.addEventListener(
-  'touchend',
-  function (event) {
-    var now = Date.now()
-    if (now - lastTouchEnd <= 300) {
-      event.preventDefault()
-    }
-    lastTouchEnd = now
-  },
-  false
-)
-document.addEventListener(
-  'touchmove',
-  function (event) {
-    if (event.scale !== 1) {
-      event.preventDefault()
-    }
-  },
-  { passive: false }
-)
+  })
+  var lastTouchEnd = 0
+  document.addEventListener(
+    'touchend',
+    function (event) {
+      var now = new Date().getTime()
+      if (now - lastTouchEnd <= 300) {
+        event.preventDefault()
+      }
+      lastTouchEnd = now
+    },
+    false
+  )
+  document.addEventListener('gesturestart', function (event) {
+    event.preventDefault()
+  })
+  document.addEventListener(
+    'touchmove',
+    function (event) {
+      if (event.scale !== 1) {
+        event.preventDefault()
+      }
+    },
+    { passive: false }
+  )
+}
 // 禁止IOS放大 End
 </script>
 
